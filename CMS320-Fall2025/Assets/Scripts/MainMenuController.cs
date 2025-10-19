8c7892f6a3a83b1e8ff48048cea8b691547a16de
Caitlyn Lefebvre <clefebvre@rollins.edu>
2025-10-19 18:16:09 -0400

    Added basic home page
    Added a home page with the title of the game and two buttons: one that will take the player to a screen with the rules and another that will take the player to the first level of the game.

---
+++ CMS320-Fall2025/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void OnRulesButtonClicked()
+    {
+        //For now, just log a message
+        Debug.Log("Rules button clicked - Scene not implemented yet.");
+    }
+
+    public void OnPlayButtonClicked()
+    {
+        Debug.Log("Play button clicked - Scene not implemented yet.");
+    }
+}

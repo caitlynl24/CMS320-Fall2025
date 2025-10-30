c46cb60f5e2c31b2fa1336944fe6c193dc2b1887
Caitlyn Lefebvre <clefebvre@rollins.edu>
2025-10-30 13:29:47 -0400

    Fixed movement

--- CMS320-Fall2025/Assets/Scripts/ScarecrowController.cs
+++ CMS320-Fall2025/Assets/Scripts/ScarecrowController.cs
@@ -5,6 +5,7 @@ public class ScarecrowController : MonoBehaviour
     public float moveSpeed = 3f;
     private Animator animator;
     private Vector2 movement;
+    private Vector2 lastMove;
 
     void Start()
     {
@@ -20,9 +21,15 @@ public class ScarecrowController : MonoBehaviour
         //Normalize diagonal movement
         movement = movement.normalized;
 
+        //Store last movement direction if moving
+        if(movement.magnitude > 0) 
+        {
+            lastMove = movement;
+        }
+
         //Update animator parameters
-        animator.SetFloat("MoveX", movement.x);
-        animator.SetFloat("MoveY", movement.y);
+        animator.SetFloat("MoveX", movement.magnitude > 0 ? movement.x : lastMove.x);
+        animator.SetFloat("MoveY", movement.magnitude > 0 ? movement.y : lastMove.y);
         animator.SetBool("IsMoving", movement.magnitude > 0);
 
         //Jump
@@ -33,5 +40,7 @@ public class ScarecrowController : MonoBehaviour
 
         //Move character
         transform.position += new Vector3(movement.x, movement.y, 0) * moveSpeed * Time.deltaTime;
+
+        Debug.Log($"MoveX: {movement.x}, MoveY: {movement.y}, IsMoving: {animator.GetBool("IsMoving")}");
     }
 }

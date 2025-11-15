f83551ad246e0d71a203214b1d458c7c1c89bb5f
Caitlyn Lefebvre <clefebvre@rollins.edu>
2025-11-15 12:28:47 -0500

    Fixed CandyManager script

--- CMS320-Fall2025/Assets/Scripts/CandyManager.cs
+++ CMS320-Fall2025/Assets/Scripts/CandyManager.cs
@@ -5,7 +5,9 @@ public class CandyManager : MonoBehaviour
 {
     public static CandyManager Instance; // Singleton for easy access
     public int candyCount = 0;
+    public int totalCandy = 0;
     public TextMeshProUGUI candyText;
+    public DoorController door; // Reference to door
 
     void Awake()
     {
@@ -16,6 +18,7 @@ public class CandyManager : MonoBehaviour
 
     void Start()
     {
+        totalCandy = FindObjectsOfType<CandyCorn>().Length;
         UpdateUI();
     }
 
@@ -23,10 +26,16 @@ public class CandyManager : MonoBehaviour
     {
         candyCount++;
         UpdateUI();
+
+        // If all candy collected, tell the door to open
+        if (candyCount >= totalCandy)
+        {
+            door.OpenDoor();
+        }
     }
 
     void UpdateUI()
     {
-        candyText.text = "Candy: " + candyCount;
+        candyText.text = "Candy: " + candyCount + " / " + totalCandy;
     }
 }

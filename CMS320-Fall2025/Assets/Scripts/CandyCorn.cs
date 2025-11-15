65706957c008c38d5261bebca427bf8ca74a1c9f
Caitlyn Lefebvre <clefebvre@rollins.edu>
2025-11-15 10:45:59 -0500

    Created the "CandyCorn" script

--- CMS320-Fall2025/Assets/Scripts/CandyCorn.cs
+++ CMS320-Fall2025/Assets/Scripts/CandyCorn.cs
@@ -1,6 +1,40 @@
 using UnityEngine;
 
-public class CandyCorn
+public class CandyCorn : MonoBehaviour
 {
-    
+    private bool playerIsNear = false;
+
+    void Update()
+    {
+        //Player must be nearby AND press C
+        if (playerIsNear && Input.GetKeyDown(KeyCode.C))
+        {
+            CollectCandy();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerIsNear = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerIsNear = false;
+        }
+    }
+
+    void CollectCandy()
+    {
+        //Add to score
+        CandyManager.Instance.AddCandy();
+
+        //Remove candy from scene
+        Destroy(gameObject);
+    }
 }

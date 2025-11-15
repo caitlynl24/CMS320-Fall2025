ee0f0529e5e35ab031305f25910f78cde4ccaad9
Caitlyn Lefebvre <clefebvre@rollins.edu>
2025-11-15 12:55:34 -0500

    Added CandyManager to the door

--- CMS320-Fall2025/Assets/Scripts/DoorController.cs
+++ CMS320-Fall2025/Assets/Scripts/DoorController.cs
@@ -1,6 +1,32 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class DoorController
+public class DoorController : MonoBehaviour
 {
-    
+    public Sprite closedSprite;
+    public Sprite openSprite;
+
+    private SpriteRenderer spriteRenderer;
+    private bool isOpen = false;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = closedSprite; //Start closed
+    }
+
+    public void OpenDoor()
+    {
+        isOpen = true;
+        spriteRenderer.sprite = openSprite;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (isOpen && other.CompareTag("Player"))
+        {
+            //Load next scene
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
 }

c72dbb5438a6eedf3d6ac98468e13f1e31b168fd
belladhamilton <ihamilton@rollins.edu>
2025-12-02 16:40:34 -0500

    Added script to StartUp Page

---
+++ CMS320-Fall2025/Assets/Scripts/StartUp.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScreen : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("HomePage");
+        }
+    }
+}
